fe4f1ba38df783cd1261da64dd0c5db55595948b
gweinrich <112568316+gweinrich@users.noreply.github.com>
2025-02-24 16:37:42 -0600

    Fixed Sidebar

--- form_portal/src/components/Sidebar/SidebarData.jsx
+++ form_portal/src/components/Sidebar/SidebarData.jsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD:form_portal/src/components/SidebarData.jsx
-import UserHomePage from "../pages/UserPages/UserHomePage";
-import UserCreateFormPage from "../pages/UserPages/UserCreateFormPage";
-import UserViewFormsPage from "../pages/UserPages/UserViewFormsPage";
-import UserSignFormsPage from "../pages/UserPages/UserSignFormsPage";
-import AdminViewFormsPage from "../pages/AdminPages/AdminViewFormPage";
-=======
-import UserHomePage from "./../../pages/UserPages/UserHomePage";
-import UserCreateFormPage from "./../../pages/UserPages/UserCreateFormPage";
-import UserViewFormsPage from "./../../pages/UserPages/UserViewFormsPage";
-import UserSignFormsPage from "./../../pages/UserPages/UserSignFormsPage";
->>>>>>> c5ae3599470e6dd511a8d0ee7bb6eae4f4a307ac:form_portal/src/components/Sidebar/SidebarData.jsx
+import UserHomePage from "../../pages/UserPages/UserHomePage";
+import UserCreateFormPage from "../../pages/UserPages/UserCreateFormPage";
+import UserViewFormsPage from "../../pages/UserPages/UserViewFormsPage";
+import UserSignFormsPage from "../../pages/UserPages/UserSignFormsPage";
+import AdminViewFormsPage from "../../pages/AdminPages/AdminViewFormPage";
 
 export const SidebarData = [
   {
