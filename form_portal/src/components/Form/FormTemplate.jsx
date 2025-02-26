db80e20766e85fc7b7bb6e96361ee9c783ae0e55
SarahMendoza <sarahmendoza@smu.edu>
2025-02-25 19:47:27 -0600

    update form component

--- form_portal/src/components/Form/FormTemplate.jsx
+++ form_portal/src/components/Form/FormTemplate.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { FormTemplateData } from "./FormTemplateData";
+import Table from "../Table";
 
-const FormTemplate = () => {
+const FormTemplate = (formTypeId) => {
+  const [filteredForms, setFilteredForms] = useState([]);
+
+  useEffect(() => {
+    // Filter the array to get only the matching form
+    const selectedForms = FormTemplateData.filter(
+      (form) => form.formTypeId === formTypeId
+    );
+    setFilteredForms(selectedForms);
+  }, [formTypeId]);
+
+  // Check if the filteredForms array is empty before rendering
+  if (filteredForms.length === 0) {
+    return <p>Form not found</p>;
+  }
+
   return (
     <div className="form-template-content">
       FORM PREVIEW
       <div className="form-template-header">Header</div>
-      <div className="form-template-applicantInfo">Applicant Info</div>
-      <div className="form-template-formContent">Form Content</div>
+      <div className="form-template-applicantInfo">Applicant Info </div>
+      <div className="form-template-formContent">content</div>
+      <Table
+        data={FormTemplateData.formType.formContent}
+        columns={FormTemplateData.formType.formContent.keys}
+      />
       <div className="form-template-signatures">Signatures</div>
     </div>
   );
--- form_portal/src/pages/UserPages/UserCreateFormPage.jsx
+++ form_portal/src/pages/UserPages/UserCreateFormPage.jsx
@@ -1,20 +1,46 @@
 // user page to create a new form, with save or submit options
 import "./UserCreateFormPage.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import FormTemplate from "../../components/Form/FormTemplate.jsx";
+import { use } from "react";
 
 const UserCreateFormPage = () => {
+  const [formTypes, setFormTypes] = useState([]);
+  const [formContents, setFormContents] = useState([]);
+  const [currentType, setCurrentType] = useState("Dropdown button");
+
   //function - API stub to fetch available form types
+  const getFormTypes = () => {
+    //fetch form types from DB
+    const form_types = [
+      { name: "Graduation", id: "GR001" },
+      { name: "Credit Petition", id: "" },
+      { name: "Research Funds Approval", id: "RSF001" },
+    ];
+    setFormTypes(form_types);
+    return;
+  };
+
+  const handleTypeSelect = (e) => {
+    console.log(e.target.value);
+    setCurrentType(e.target.value);
+  };
 
   //function - API stub to fetch needed form contents (or, store this from the form type call)
-
+  const getFormContents = (formTypeId) => {
+    //fetch form contents from DB
+  };
+  useEffect(() => {
+    getFormTypes();
+  }, []);
   //function - API stub for submitting form, sending data to DB backend
 
   return (
     <div className="main-page-content">
       <h2>Create Form</h2>
+      <p>{currentType}</p>
       <p>
         Fill out a new digital form by selecting from the options below. Make
         sure to add your required signees and complete all form contents.
@@ -22,10 +48,21 @@ const UserCreateFormPage = () => {
       <div>
         <p>Select form type:</p>
         <div className="inline-container">
-          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <DropdownButton
+            id="dropdown-basic-button"
+            title={currentType}
+            value={currentType}
+            onChange={handleTypeSelect}
+          >
+            {
+              /*  This maps each array item to a div adds
+                the style declared above and return it */
+              formTypes.map((item, index) => (
+                <Dropdown.Item key={index}>
+                  {item.name} -- {index}
+                </Dropdown.Item>
+              ))
+            }
           </DropdownButton>
           <span>Form ID: [insert form ID from state here]</span>
         </div>
@@ -39,7 +76,7 @@ const UserCreateFormPage = () => {
       {/* add editable table here */}
       <p>Form preview:</p>
       {/* display dynamic form component here */}
-      <FormTemplate />
+      <FormTemplate formTypeId={currentType} />
       {/* add "Save Form" button */}
       {/* add "Submit" button */}
     </div>
